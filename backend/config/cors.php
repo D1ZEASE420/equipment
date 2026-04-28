31115364a4dfa3175bcb12ca780dad9c868fdec7
D1ZEASE420 <heinlaidjuhan@gmail.com>
2026-04-28 09:19:33 +0300

    fix: add production domain to CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -7,6 +7,7 @@ return [
         'http://localhost:5173',
         'http://localhost:3000',
         'http://127.0.0.1:5173',
+	'https://tak23heinlaid.itmajakas.ee',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
